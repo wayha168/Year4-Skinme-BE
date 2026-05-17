76e2e0c08cca736739bda3261560a94eebcda36c
wayha168 <vehaseng1122@example.com>
2026-05-17 19:41:18 +0700

    update POS with cash payment

--- src/main/resources/db/fix-payments-columns.sql
+++
@@ -1,4 +0,0 @@
--- Run on skinme_db if POS cash / pickup payment fails with:
--- "Data truncated for column 'method' at row 1"
-ALTER TABLE payments MODIFY COLUMN method VARCHAR(32) NOT NULL;
-ALTER TABLE payments MODIFY COLUMN status VARCHAR(32) NOT NULL;
